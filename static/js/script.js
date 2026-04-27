00d0cb80d98f50d5523d00b176477f13ec16f2bf
Project Ridwan <ridwanarifinshoheh01@gmail.com>
2026-04-27 12:25:27 +0700

    update

--- static/js/script.js
+++ static/js/script.js
@@ -1,48 +1,15 @@
-window.addEventListener("scroll", function() {
-  let reveals = document.querySelectorAll(".reveal");
+document.addEventListener("DOMContentLoaded", function () {
 
-  reveals.forEach(el => {
-    let windowHeight = window.innerHeight;
-    let elementTop = el.getBoundingClientRect().top;
+  let slides = document.querySelectorAll(".slide");
+  let index = 0;
 
-    if (elementTop < windowHeight - 100) {
-      el.classList.add("active");
-    }
-  });
+  function showSlide() {
+    slides.forEach(s => s.classList.remove("active"));
+    slides[index].classList.add("active");
+
+    index = (index + 1) % slides.length;
+  }
+
+  setInterval(showSlide, 4000);
+
 });
-
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    }
-  });
-});
-
-document.querySelectorAll(".fade-up").forEach(el => {
-  observer.observe(el);
-});
-
-let slides = document.querySelectorAll(".slide");
-let index = 0;
-
-function showSlide() {
-  slides.forEach(slide => slide.classList.remove("active"));
-  slides[index].classList.add("active");
-
-  index++;
-  if (index >= slides.length) index = 0;
-}
-
-// auto slide tiap 4 detik
-setInterval(showSlide, 4000);
-
-let slides = document.querySelectorAll(".slide");
-let index = 0;
-
-setInterval(() => {
-  slides.forEach(s => s.classList.remove("active"));
-  slides[index].classList.add("active");
-
-  index = (index + 1) % slides.length;
-}, 4000);
